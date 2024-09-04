a08a66da9d8db89e7f39af06538ce91850339596
Hera Sadikaj <hera.sadikaj@yahoo.com>
2024-09-04 14:10:01 +0200

    changes

--- src/pages/firstpage.jsx
+++ src/pages/firstpage.jsx
@@ -1,24 +1,24 @@
-//import React from 'react';
+/* eslint-disable no-unused-vars */
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './pages.css';
+import backgroundImage from './back.jpg'; // Update path accordingly
 
 const Firstpage = () => {
     return (
-        <div className='main'>
-        <div className="left-container">
-            <h1>NOTES</h1>
-            <div className="button-container">
-                <Link to="login">
-                    <button>Login</button>
-                </Link>
-                <Link to="/register">
-                    <button>Register</button>
-                </Link>
+        <div className='main' style={{ backgroundImage: `url(${backgroundImage})` }}>
+            <div className="left-container">
+                <h1>NOTES</h1>
+                <div className="button-container">
+                    <Link to="login">
+                        <button>Login</button>
+                    </Link>
+                    <Link to="/register">
+                        <button>Register</button>
+                    </Link>
+                </div>
             </div>
         </div>
-        <img src='src/pages/notes.png' alt='Notes' width={400} height={650} />
-    </div>
-    
     );
 };
 
